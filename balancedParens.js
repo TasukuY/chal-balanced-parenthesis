ed2a01a7b41cd0ba414e32410c9cfb6d7ef33471
TasukuY <alohaty81@gmail.com>
2022-02-27 22:30:18 -0700

    coding challenge done

--- balancedParens.js
+++ balancedParens.js
@@ -5,3 +5,19 @@ let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
 // Write your solution below:
+
+let has_balanced_parens = word => {
+    let openParensCounter = 0;
+    let closeParensCounter = 0;
+
+    for(let i = 0; i < word.length; i++){
+        if(word[i] === '('){
+            openParensCounter++;
+        }else if(word[i] === ')'){
+            closeParensCounter++;
+        }
+    }
+    return openParensCounter === closeParensCounter;
+}
+
+console.log(has_balanced_parens(sample4));
